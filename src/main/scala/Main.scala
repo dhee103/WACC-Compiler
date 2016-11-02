f6e5ac1ab61874f2337892e193ee8f86fdb0ef8a
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-02 18:19:00 +0000

    [RC] [HM] Added code to determine token type of input file.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,11 +15,19 @@ object Main {
     // Get a list of matched tokens
     var tokens = new org.antlr.v4.runtime.CommonTokenStream(waccLex)
 
+    val tokenIDs : Array[String] =  waccLex.getRuleNames()
+
     println("The tokens are " + tokens.getText())
 
     println("tokens size is " + tokens.size())
 
     for (i <- 0 until tokens.size()) {
+      println("yo")
+      println("token " + i  + " is " + tokens.get(i).getText() + " of type " + mapToId(tokens.get(i).getType(), tokenIDs))
+    }
+
+    for (i <- 0 until tokens.size()) {
+      println("no")
       println("token " + i  + " is " + tokens.get(i).getText() + " of type " + tokens.get(i).getType())
     }
 
@@ -28,6 +36,10 @@ object Main {
   def factorial(n: Int): Int = n match {
     case 0 => 1
     case _ => n * factorial(n - 1)
+  }
+
+  def mapToId(typeNum: Int, tokenNames: Array[String]): String = {
+    if (typeNum > 0) tokenNames(typeNum - 1) else "EOF"
   }
 
 }
