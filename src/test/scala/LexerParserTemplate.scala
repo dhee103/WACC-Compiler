a9bf6e45d5488f8cd09732e862ea847eddbf2ffe
dsg115 <dsg115>
2016-11-03 17:08:10 +0000

    [DG] few small refactorings need to add more at home

--- src/test/scala/LexerParserTemplate.scala
+++ src/test/scala/LexerParserTemplate.scala
@@ -1,8 +1,6 @@
-import org.antlr.v4.runtime.ANTLRFileStream._
-import org.antlr.v4.runtime.CommonTokenStream._
-import org.antlr.v4.runtime.Token._
 
-class LexerParsertemplate(val filename : String){
+
+class LexerParserTemplate(val filename: String){
 
   var waccLex = new WaccLexer(new org.antlr.v4.runtime.ANTLRFileStream(filename))
 
@@ -19,32 +17,22 @@ class LexerParsertemplate(val filename : String){
 
   private def constructTypeNumberArrays(xs : org.antlr.v4.runtime.CommonTokenStream): Array[Int] = {
 
-    var types : Array[Int] = new Array[Int](xs.size())
-
-    for(i <- 0 to (types.length - 1)){
-
-      types(i) = tokens.get(i).getType()
-
+    val types: Array[Int] = new Array[Int](xs.size())
+    for(i <- types.indices){
+      types(i) = tokens.get(i).getType
     }
-
-    return types
+    types
 
   }
 
   private def ConstructTokenStringArray(xs : Array[Int]): Array[String] = {
-
-      return xs.map(mapToId(_, tokenIDs))
+      xs.map(mapToId(_, tokenIDs))
   }
 
   def getLexerResult : Array[String] = {
-
     val typeList = constructTypeNumberArrays(tokens)
-
     val tokenList = ConstructTokenStringArray(typeList)
-
-    return tokenList
-
+    tokenList
   }
-
 
 }
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,26 +1,16 @@
 import org.scalatest._
 //TODO: be more selective with the imports
+//TODO: check if matchers works at home
 
 class MainTest extends FlatSpec with Matchers{
 
-  "tokens" should "be correct" in {
-
-    val lex1 : LexerParsertemplate = new LexerParsertemplate("WaccTestFiles/basicSeq.wacc")
-
-    val tokenList = lex1.getLexerResult
-
-    val excpectedList : Array[String] = Array("BEGIN", "SKIP", "SEMICOLON", "SKIP", "END", "EOF")
-
-    assert(tokenList === excpectedList)
-  }
-
   "toks" should "work" in {
-    val files = Array("WaccTestFiles/basicSeq.wacc")
-    val expectedTokens = Array(Array("BEGIN", "SKIP", "SEMICOLON", "SKIP",
-      "END","EOF"))
+    val files: Array[String] = Array("WaccTestFiles/basicSeq.wacc")
+    val expectedTokens: Array[Array[String]] = Array(Array("BEGIN", "SKIP",
+      "SEMICOLON", "SKIP", "END","EOF"))
 
-    (files zip expectedTokens).map{ case (file, tokens) => assert(new LexerParsertemplate(file).getLexerResult === tokens)}
-
+    (files zip expectedTokens).map{ case (file, tokens) =>
+      assert(new LexerParserTemplate(file).getLexerResult === tokens)}
   }
 
 }
