53baa953378c1918945ccd8a72153948fc1722fb
Harry <harryonline3@hotmail.co.uk>
2016-11-05 17:24:13 +0000

    Fixed some small errors in TreeNode class

--- src/main/scala/TreeNode.scala
+++ src/main/scala/TreeNode.scala
@@ -1,10 +1,10 @@
-class TreeNode[T] (val: _item: T, val _left : Node[T],
- val _right: Node[T], val _parent: Node[T]){
+class TreeNode[T] (val _item: T, val _left: TreeNode[T],
+ val _right: TreeNode[T], val _parent: TreeNode[T]){
 
    private var item: T = _item
-   private var left: Node[T] = _left
-   private var right: Node[T] = _right
-   private var parent: Node[t] = _parent
+   private var left: TreeNode[T] = _left
+   private var right: TreeNode[T] = _right
+   private var parent: TreeNode[T] = _parent
 
    def setItem(value : T): Unit = {
       item = value
@@ -18,10 +18,10 @@ class TreeNode[T] (val: _item: T, val _left : Node[T],
 
     def getParentNode = parent
 
-    def setLeft(newNode : Node[T]): Unit
+    def setLeft(newNode: TreeNode[T]): Unit
       = left = newNode
 
-    def setRight(newNode : Node[T]): Unit
+    def setRight(newNode: TreeNode[T]): Unit
       = right = newNode
 
     def print: T
@@ -31,11 +31,11 @@ class TreeNode[T] (val: _item: T, val _left : Node[T],
 
 trait GenericTree[T] {
 
-  private var head: Node[T]
+  var head: TreeNode[T]
 
-  def add(node : Node[T]): Unit
+  def add(node: TreeNode[T]): Unit
   def toPreOrderList: Array[T]
   def toInOrderList: Array[T]
-  def toPostOrderListL Array[T]
-  
+  def toPostOrderList: Array[T]
+
 }
