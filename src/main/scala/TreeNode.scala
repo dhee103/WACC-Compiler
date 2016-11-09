9954c3bccae9ffee2624410116a8f90b26c0fd71
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-09 15:18:43 +0000

    [RC] Fixed spacing issues.

--- src/main/scala/TreeNode.scala
+++ src/main/scala/TreeNode.scala
@@ -1,21 +1,21 @@
-class TreeNode[T] (val _item: T, val _children: Array[TreeNode[T]], val _parent: TreeNode[T]){
+class TreeNode[T](val _item: T, val _children: Array[TreeNode[T]], val _parent: TreeNode[T]) {
 
-   private var item: T = _item
+  private var item: T = _item
 
-   private var parent: TreeNode[T] = _parent
+  private var parent: TreeNode[T] = _parent
 
-   private var children: Array[TreeNode[T]] = _children
+  private var children: Array[TreeNode[T]] = _children
 
-   def setItem(value : T): Unit = {
-      item = value
-    }
+  def setItem(value: T): Unit = {
+    item = value
+  }
 
-    def getItem = item
+  def getItem = item
 
-    def getParentNode = parent
+  def getParentNode = parent
 
-    def print: T
-      = item
+  def print: T
+  = item
 
 }
 
@@ -24,8 +24,11 @@ trait GenericTree[T] {
   var head: TreeNode[T]
 
   def add(node: TreeNode[T]): Unit
+
   def toPreOrderList: Array[T]
+
   def toInOrderList: Array[T]
+
   def toPostOrderList: Array[T]
 
 }
