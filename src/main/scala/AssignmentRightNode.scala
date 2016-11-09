67c37837e8405fbac98c217fabb0f44ea6c9b43a
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-09 15:50:18 +0000

    [RC] [DG] Added fields to AssignmentRightNode classes.

--- src/main/scala/AssignmentRightNode.scala
+++ src/main/scala/AssignmentRightNode.scala
@@ -3,9 +3,11 @@ trait AssignmentRightNode extends AstNode {
 }
 
 class NewPairNode extends AssignmentRightNode {
-
+  val fstElem: ExprNode
+  val sndElem: ExprNode
 }
 
 class CallNode extends AssignmentRightNode {
-
+  val id: IdentNode
+  val argList: Option[ArgListNode]
 }
