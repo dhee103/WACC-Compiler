90cbbc4bc1f6a36bbe9636324cd78083c769d7bd
Harry Moore <hrm15@line02.doc.ic.ac.uk>
2016-11-10 16:36:28 +0000

    [HM] [DG] Implemented type checking helper methods in annotate class

--- src/main/scala/Annotate.scala
+++ src/main/scala/Annotate.scala
@@ -1,7 +1,31 @@
+import scala.reflect.ClassTag
+import scala.reflect.runtime.{universe => univ}
+
 class Annotate(val _AST: ProgNode) {
 
   private var AST = _AST
 
   //def getAnnotatedAST
+
+  def annotateStatNode(statement: StatNode): Unit = {
+
+  
+
+  }
+
+  def getType[T](obj: T): String = {
+    val str: String = obj.getClass().toString
+    str.substring(6, str.length)
+  }
+
+  def checkNodesHaveSameType(typeNode1: TypeNode, typeNode2: TypeNode): Boolean = {
+    def f[A, B: ClassTag](a: A, b: B) = a match {
+      case _: B => true
+      case _    => false
+    }
+    f(typeNode1, typeNode2)
+  }
+
+
 
 }
