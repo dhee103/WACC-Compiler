a46bfc6f6c9fb0044aef558e462cc77520724754
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-11 00:36:22 +0000

    [RC] Changed firstArg and secondArg fields in BinaryOperationNode to leftExpr and rightExpr, respectively.

--- src/main/scala/BinaryOperationNode.scala
+++ src/main/scala/BinaryOperationNode.scala
@@ -1,58 +1,58 @@
 trait BinaryOperationNode extends ExprNode {
 
-  val firstArg: ExprNode
-  val secondArg: ExprNode
+  val leftExpr: ExprNode
+  val rightExpr: ExprNode
 
 }
 
-class MulOperationNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class MulOperationNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class DivOperationNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class DivOperationNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class ModNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class ModNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class PlusNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class PlusNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class MinusNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class MinusNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class GreaterThanNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class GreaterThanNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class GreaterEqualNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class GreaterEqualNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class LessThanNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class LessThanNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class LessEqualNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class LessEqualNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class DoubleEqualNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class DoubleEqualNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class NotEqualNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class NotEqualNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class LogicalAndNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class LogicalAndNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
 
-class LogicalOrNode(val firstArg: ExprNode, val secondArg: ExprNode) extends BinaryOperationNode {
+class LogicalOrNode(val leftExpr: ExprNode, val rightExpr: ExprNode) extends BinaryOperationNode {
 
 }
