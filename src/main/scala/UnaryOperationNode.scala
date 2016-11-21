bfedb97826186906acd1a0dd0ddd110126f27db6
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-21 19:23:02 +0000

    [RC] [DG] Implemented getType method for OrdNode.

--- src/main/scala/UnaryOperationNode.scala
+++ src/main/scala/UnaryOperationNode.scala
@@ -38,7 +38,14 @@ case class LenNode(override val argument: ExprNode) extends UnaryOperationNode {
 }
 
 case class OrdNode(override val argument: ExprNode) extends UnaryOperationNode {
-
+  override def getType: TypeNode = {
+    if (argument.getType == CharTypeNode()) {
+      IntTypeNode()
+    } else {
+      ErrorLog.add("[Semantic Error]: Argument of bool type expected in logical not operation.")
+      ErrorTypeNode()
+    }
+  }
 }
 
 case class ChrNode(override val argument: ExprNode) extends UnaryOperationNode { }
