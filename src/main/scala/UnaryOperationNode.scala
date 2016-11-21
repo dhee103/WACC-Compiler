4c204538444c15649b322abbff1124a50c3b571c
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-21 19:19:11 +0000

    [RC] [DG] Implemented getType method for LogicalNotNode.

--- src/main/scala/UnaryOperationNode.scala
+++ src/main/scala/UnaryOperationNode.scala
@@ -4,7 +4,16 @@ trait UnaryOperationNode extends ExprNode {
 
 }
 
-case class LogicalNotNode(override val argument: ExprNode) extends UnaryOperationNode { }
+case class LogicalNotNode(override val argument: ExprNode) extends UnaryOperationNode {
+  override def getType: TypeNode = {
+    if (argument.getType == BoolTypeNode()) {
+      BoolTypeNode()
+    } else {
+      ErrorLog.add("[Semantic Error]: Argument of bool type expected in logical not operation.")
+      ErrorTypeNode()
+    }
+  }
+}
 
 case class NegativeNode(override val argument: ExprNode) extends UnaryOperationNode { }
 
