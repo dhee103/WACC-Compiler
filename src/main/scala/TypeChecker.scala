e819eeeb6aa53e77b5a93f3fd836cddca496c6af
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-22 00:18:00 +0000

    [RC] Created isArrayLiteralValid function.
    Changed return type of checkDeclaration from Boolean to (Boolean, String)

--- src/main/scala/TypeChecker.scala
+++ src/main/scala/TypeChecker.scala
@@ -1,30 +1,24 @@
 object TypeChecker {
 
-  def checkDeclaration(declarationNode: DeclarationNode): Boolean = {
+  def checkDeclaration(declarationNode: DeclarationNode): (Boolean, String) = {
     val ident = declarationNode.identifier
     val rhs = declarationNode.rhs
 
-    // TO DO: Checking if each element of an array is of same type
-    rhs match {
-      case rhs: ArrayLiteralNode =>
-        if (rhs.values.nonEmpty) {
-        rhs.values.forall(expr => expr.getType ==
-          rhs.values
-          .head
-          .getType)
-      }
+    if (!isArrayLiteralValid(rhs)) {
+      return (false, "[Semantic Error] Array literal contains expressions of differing types.")
     }
 
     val identType: TypeNode = ident.getType
     val rhsType: TypeNode = rhs.getType
 
     rhsType match {
-      case ArrayTypeNode(null) => identType.isInstanceOf[ArrayTypeNode]
-      case _                   => rhsType == identType
+      case ArrayTypeNode(null) => (identType.isInstanceOf[ArrayTypeNode], "[Semantic Error] Type mismatch in declaration statement.")
+      case _ => (rhsType == identType, "[Semantic Error] Type mismatch in declaration statement.")
     }
 
   }
 
+  // TO DO: Redo this entire function.
   def checkAssignment(assignmentNode: AssignmentNode) = {
     val lhs = assignmentNode.lhs
     val rhs = assignmentNode.rhs
@@ -33,6 +27,20 @@ object TypeChecker {
       case lhs: IdentNode => lhs.getType
       case lhs: PairElemNode => lhs.getType
       case lhs: ArrayElemNode => lhs.identifier.getType // finish this .unwrap
+    }
+  }
+
+  // isArrayLiteralValid is true iff all expressions in array literal on rhs
+  // have same type
+  def isArrayLiteralValid(rhs: AssignmentRightNode): Boolean = {
+    rhs match {
+      case ArrayLiteralNode(values) =>
+        if (values.nonEmpty) {
+          values.forall(expr => expr.getType == values.head.getType)
+        } else {
+          true
+        }
+      case _ => true
     }
   }
 }
