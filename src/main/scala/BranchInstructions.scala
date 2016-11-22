9107cc921e8a2abe1303e510de2a16be20a23178
Harry Moore <hrm15@matrix37.doc.ic.ac.uk>
2016-11-22 14:53:05 +0000

    [HM] Completed toString for registers

--- src/main/scala/BranchInstructions.scala
+++ src/main/scala/BranchInstructions.scala
@@ -1,7 +1,10 @@
 case class Jump(val goToLabel: String) extends Instruction {
 
+  override def toString() = "BL " + goToLabel
+
 }
 
 case class BranchEqual() extends Instruction {
+  //todo
 
 }
--- src/main/scala/Register.scala
+++ src/main/scala/Register.scala
@@ -16,12 +16,18 @@ case class BasePointer() extends Register {
 
 case class LinkRegister() extends Register {
 
+  override def toString() = "lr"
+
 }
 
 case class ProgramCounter() extends Register {
 
+  override def toString() = "pc"
+
 }
 
 case class ResultRegister() extends Register {
+
+  override def toString() = "R0"
 
 }
