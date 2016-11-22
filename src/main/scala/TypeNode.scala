f5cd4f7d97accc7a44aac2fa32be62ecca4a0c5d
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-22 02:12:43 +0000

    [RC] Added toTypeNode method to PairElemTypeNode trait.

--- src/main/scala/TypeNode.scala
+++ src/main/scala/TypeNode.scala
@@ -4,6 +4,11 @@ trait TypeNode extends AstNode {
 
 trait BaseTypeNode extends TypeNode with PairElemTypeNode {
   override def toPairElemTypeNode: PairElemTypeNode = this
+  override def toTypeNode: TypeNode = this
+}
+
+trait PairElemTypeNode extends AstNode {
+  def toTypeNode: TypeNode
 }
 
 case class IntTypeNode() extends BaseTypeNode {
@@ -20,18 +25,18 @@ case class StringTypeNode() extends BaseTypeNode {
 
 case class ArrayTypeNode(val elemType: TypeNode) extends PairElemTypeNode with TypeNode {
   override def toPairElemTypeNode: PairElemTypeNode = this
+  override def toTypeNode: TypeNode = this
 }
 
 case class PairTypeNode(val firstElemType: PairElemTypeNode, val secondElemType: PairElemTypeNode) extends TypeNode {
   override def toPairElemTypeNode: PairElemTypeNode = InnerPairTypeNode()
 }
 
-trait PairElemTypeNode extends AstNode {
-}
-
 case class InnerPairTypeNode() extends PairElemTypeNode {
+  override def toTypeNode: TypeNode = PairTypeNode(null, null) // watch out for this
 }
 
-case class ErrorTypeNode() extends TypeNode {
+case class ErrorTypeNode() extends TypeNode with PairElemTypeNode {
   override def toPairElemTypeNode: PairElemTypeNode = this // does this cause any problems?
+  override def toTypeNode: TypeNode = this
 }
