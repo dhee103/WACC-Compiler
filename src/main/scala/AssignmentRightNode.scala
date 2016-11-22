bf7ff27d7dec9b5bc258bd7c97cce9c157e57059
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-22 03:14:00 +0000

    [RC] Updated getType method in CallNode such that it checks the correct number of parameters are provided and each parameter has the correct type as dictated by the FunctionTable.

--- src/main/scala/AssignmentRightNode.scala
+++ src/main/scala/AssignmentRightNode.scala
@@ -6,11 +6,25 @@ case class NewPairNode(val fstElem: ExprNode, val sndElem: ExprNode) extends Ass
   override def getType: TypeNode = {
     val fstElemType = fstElem.getType
     val sndElemType = sndElem.getType
-    // TO DO: Write our own function to get PairElemTypeNode from TypeNode
-    new PairTypeNode(fstElemType.toPairElemTypeNode, sndElemType.toPairElemTypeNode)
+    PairTypeNode(fstElemType.toPairElemTypeNode, sndElemType.toPairElemTypeNode)
   }
 }
 
 case class CallNode(val id: IdentNode, val argList: Option[ArgListNode]) extends AssignmentRightNode {
-  override def getType: TypeNode = FunctionTable.getReturnType(id)
+  override def getType: TypeNode = {
+    val paramTypeList: IndexedSeq[TypeNode] = FunctionTable.getParamTypes(id)
+
+    val argListMatchesParamList: Boolean = argList match {
+      case Some(list) => list.exprs.length == paramTypeList.length &&
+        ((list.exprs, paramTypeList).zipped map(_ == _)).forall(identity)
+      case None => paramTypeList.isEmpty
+    }
+
+    if (argListMatchesParamList) {
+      FunctionTable.getReturnType(id)
+    } else {
+      ErrorLog.add("[Semantic Error] Invalid parameters provided to function call.")
+      ErrorTypeNode()
+    }
+  }
 }
