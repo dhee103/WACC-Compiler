283eaa6a336a0f9bc486b7944dd11973b1051423
Ruhi Choudhury <arc13@ic.ac.uk>
2016-11-22 03:53:46 +0000

    [RC] Implemented checkAssignment function.

--- src/main/scala/TypeChecker.scala
+++ src/main/scala/TypeChecker.scala
@@ -14,20 +14,24 @@ object TypeChecker {
     (rhsType.isEquivalentTo(identType), "[Semantic Error] Type mismatch in declaration statement.")
   }
 
-  // TO DO: Redo this entire function.
-  def checkAssignment(assignmentNode: AssignmentNode) = {
+  def checkAssignment(assignmentNode: AssignmentNode): (Boolean, String) = {
     val lhs = assignmentNode.lhs
     val rhs = assignmentNode.rhs
 
-    val lhsType = lhs match {
-      case lhs: IdentNode => lhs.getType
-      case lhs: PairElemNode => lhs.getType
-      case lhs: ArrayElemNode => lhs.identifier.getType // finish this .unwrap
+    if (!isArrayLiteralValid(rhs)) {
+      return (false, "[Semantic Error] Array literal contains expressions of differing types.")
+    }
+
+    val lhsType: TypeNode = lhs.getType
+    val rhsType: TypeNode = rhs.getType
+
+    (rhsType.isEquivalentTo(lhsType), "[Semantic Error] Type mismatch in assignment statement.")
     }
   }
 
   // isArrayLiteralValid is true iff all expressions in array literal on rhs
-  // have same type
+  // have same type.
+  // An empty array literal is always valid.
   def isArrayLiteralValid(rhs: AssignmentRightNode): Boolean = {
     rhs match {
       case ArrayLiteralNode(values) =>
