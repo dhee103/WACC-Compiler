bea92f9596f425091a7c29870212d86d840b2ff3
dsg115 <dsg115>
2016-11-25 11:41:15 +0000

    [DG] Added "BuiltInFunctions.scala" to refer to the built in functions

---
+++ src/main/scala/BuiltInFunctions.scala
@@ -0,0 +1,58 @@
+import scala.collection.mutable._
+
+object BuiltInFunctions {
+
+  val lr = LinkRegister()
+  val r0  = ResultRegister()
+  val r1 = R1()
+  val pc  = ProgramCounter()
+
+  val zero = ImmNum(0)
+  val loadZero = LoadImmNum(0)
+  val pushlr = Push(lr)
+  val poppc = Pop(pc)
+  val ltorg = Ltorg()
+
+// make a hashmap of all the built in messages
+// generate label from "Labels.scala" as appropriate within functions
+
+  // msg_1:
+	// 	.word 3
+	// 	.ascii	"%d\0"
+	// msg_2:
+	// .word 1
+	// 	.ascii	"\0"
+	// msg_3:
+	// 	.word 5
+	// 	.ascii	"%.*s\0"
+
+
+  def println(): List[Instruction] = {
+    // generate label
+    // pushlr :: Move(r1, r0) :: Load(r0, )
+    Nil
+  }
+
+  def printInt(): List[Instruction] = {
+    Nil
+  }
+
+  // p_print_int:
+	// 	PUSH {lr}
+	// 	MOV r1, r0
+	// 	LDR r0, =msg_0
+	// 	ADD r0, r0, #4
+	// 	BL printf
+	// 	MOV r0, #0
+	// 	BL fflush
+	// 	POP {pc}
+	// p_print_ln:
+	// 	PUSH {lr}
+	// 	LDR r0, =msg_1
+  // 	ADD r0, r0, #4
+	// 	BL puts
+	// 	MOV r0, #0
+	// 	BL fflush
+	// 	POP {pc}
+
+}
