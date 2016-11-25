a33bf5a04d5dc8a53139614c56e7f5d37f36544d
dsg115 <dsg115>
2016-11-25 11:37:49 +0000

    [DG] Updated HashMaps to LinkedHashMaps to preserve insertion order. Changed getters and setters to reflect this

--- src/main/scala/Labels.scala
+++ src/main/scala/Labels.scala
@@ -4,8 +4,10 @@ object Labels {
 
   var stream: Stream[Int] = Stream.from(1)
 
-  val msgMap = HashMap[String, MutableList[String]]()
+  val msgMap = LinkedHashMap[String, MutableList[String]]()
   // val msgs = MutableList[String]()
+
+  val dataMsgMap = LinkedHashMap[String, MutableList[String]]()
 
   def getLabel(str:String): (String, String) = {
 
@@ -15,12 +17,33 @@ object Labels {
     (str ++ head.toString() ++ "start ", str ++ head.toString() ++ "end")
   }
 
+// TODO: Add parameter for name
   def addMessageLabel(str: String): Unit = {
     msgMap += ("msg_" + str ->
       MutableList[String](".word " + str.size, ".ascii \"" + str + "\""))
   }
 
   def getMessageLabel(): String = {
-    msgMap.head._1
+    msgMap.last._1
   }
+
+  def addDataMsgLabel(str: String): Unit = {
+    dataMsgMap += ("msg_" + str ->
+      MutableList[String](".word " + str.size, ".ascii \"" + str + "\""))
+      // TODO: make sure str is below n characters
+  }
+
+  def getDataMsgLabel(): String = {
+    dataMsgMap.last._1
+  }
+
+  def printDataMsgMap(): List[Instruction] = {
+    if (dataMsgMap.isEmpty) {
+      Nil
+    } else {
+      Directive(".data\n") :: Label(dataMsgMap.map(pair => pair._1 + "\n" + pair._2.mkString).mkString) :: Nil
+    }
+
+  }
+
 }
