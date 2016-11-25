48884c3d9fe7487c2368c2a32d249e2aca990bf3
dsg115 <dsg115>
2016-11-25 11:49:17 +0000

    [DG] Overloaded "addMessageLabel" and "addDataMsgLabel" in "Labels" to allow for better label names. Previously was just the string but now can be anything

--- src/main/scala/Labels.scala
+++ src/main/scala/Labels.scala
@@ -23,12 +23,23 @@ object Labels {
       MutableList[String](".word " + str.size, ".ascii \"" + str + "\""))
   }
 
+  def addMessageLabel(str: String, name: String): Unit = {
+    msgMap += ("msg_" + name ->
+      MutableList[String](".word " + str.size, ".ascii \"" + str + "\""))
+  }
+
   def getMessageLabel(): String = {
     msgMap.last._1
   }
 
   def addDataMsgLabel(str: String): Unit = {
     dataMsgMap += ("msg_" + str ->
+      MutableList[String](".word " + str.size, ".ascii \"" + str + "\""))
+      // TODO: make sure str is below n characters
+  }
+
+  def addDataMsgLabel(str: String, name: String): Unit = {
+    dataMsgMap += ("msg_" + name ->
       MutableList[String](".word " + str.size, ".ascii \"" + str + "\""))
       // TODO: make sure str is below n characters
   }
@@ -43,7 +54,6 @@ object Labels {
     } else {
       Directive(".data\n") :: Label(dataMsgMap.map(pair => pair._1 + "\n" + pair._2.mkString).mkString) :: Nil
     }
-
   }
 
 }
