589c576ef814bd5e30bcf6f47139150c995e8420
arc13 <arc13@ic.ac.uk>
2016-12-02 23:42:16 +0000

    [DG] Created file to store all constants.

---
+++ src/main/scala/Constants.scala
@@ -0,0 +1,17 @@
+object Constants {
+  //todo: import this everywhere
+  val WORD_SIZE = 4
+
+  val lr = LinkRegister()
+  val r0 = ResultRegister()
+  val r1 = R1()
+  val pc = ProgramCounter()
+  val fp = FramePointer()
+  val sp = StackPointer()
+
+  val zero = ImmNum(0)
+  val loadZero = LoadImmNum(0)
+  val pushlr = Push(lr)  //todo: remove these unneeded variables
+  val poppc = Pop(pc)
+  val ltorg = Directive("ltorg")
+}
