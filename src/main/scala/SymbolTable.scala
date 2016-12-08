f033a97e03f68b4acb00133b6bea742c05046990
Ruhi Choudhury <arc13@ic.ac.uk>
2016-12-08 05:58:49 +0000

    [RC] Undid commit 1d8e3e01.

--- src/main/scala/SymbolTable.scala
+++ src/main/scala/SymbolTable.scala
@@ -1,23 +1,23 @@
 import collection.mutable.HashMap
 
-// A symbol table maps NAMES of identifiers to their TYPES
 class SymbolTable(val encTable: Option[SymbolTable]) {
 
-  val dict: HashMap[String, TypeNode] = new HashMap[String, TypeNode]()
+  val dict: HashMap[IdentNode, TypeNode] = new HashMap[IdentNode, TypeNode]()
 
-  def add(identifier: IdentNode, typeInfo: TypeNode): Unit = {
-    dict += (identifier.name -> typeInfo)
-  }
+  def add(identifier: IdentNode, typeInfo: TypeNode): Unit
+    = dict += (identifier -> typeInfo)
 
   def lookup(identifier: IdentNode): TypeNode = {
-    dict.getOrElse(identifier.name, ErrorTypeNode())
+    dict.getOrElse(identifier, ErrorTypeNode())
   }
 
   def lookupAll(identifier: IdentNode): TypeNode = {
+
     encTable match {
       case None => lookup(identifier)
-      case Some(parentTable: SymbolTable) => dict.getOrElse(identifier.name, parentTable.lookupAll(identifier))
+      case Some(parentTable: SymbolTable) => dict.getOrElse(identifier, parentTable.lookupAll(identifier))
     }
+
   }
 
   def doesContain(identifier: IdentNode): Boolean = {
@@ -29,7 +29,7 @@ class SymbolTable(val encTable: Option[SymbolTable]) {
   }
 
   def symbols: List[IdentNode] = {
-    (for ((name, typeNode) <- dict) yield IdentNode(name, Some(typeNode))).to[collection.immutable.List]
+    (for (pair <- dict) yield pair._1).to[collection.immutable.List]
   }
 
 }
